9bbdfa396ef4e5844584ab32a159b4e34addda38
Jens Cappelle <j.cappelle@wimionline.be>
2019-11-11 16:19:43 +0100

    initial

---
+++ main.js
@@ -0,0 +1,38 @@
+const fastify = require('fastify')({
+  logger: true,
+});
+
+// Validate incoming data
+const opts = {
+  schema: {
+    body: {
+      type: 'object',
+      properties: {
+        someKey: { type: 'string' },
+        someOtherKey: { type: 'number' },
+      },
+    },
+  },
+};
+
+fastify.register(require('fastify-jwt'), {
+  secret: process.env.SECRET || 'youshouldspecifyalongsecret',
+});
+
+fastify.get('/', async () => ({ hello: 'world' }));
+
+fastify.post('/', opts, async () => ({ hello: 'world' }));
+
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
+
+// Task 1: create a package.json that includes the required Node.js modules
+// https://docs.npmjs.com/creating-a-package-json-file
+// Use `npm init`
